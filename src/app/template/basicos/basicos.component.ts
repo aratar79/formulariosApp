6664dc70dcb04af1231b2aa5f8ee757f4b9564ae
jordizurita <jordi.zurita@alimentium.com>
2022-08-13 11:22:13 +0200

    first part template forms

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styleUrls: ['./basicos.component.css']
+})
+export class BasicosComponent implements OnInit {
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  validacionNombre(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid &&
+           this.miFormulario?.controls['producto']?.touched
+  }
+
+  validacionPrecio(): boolean {
+    //return !this.miFormulario?.controls['precio']?.invalid &&
+    return  this.miFormulario?.controls['precio']?.touched &&
+            this.miFormulario?.controls['precio']?.value < 0
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value)
+  }
+}
