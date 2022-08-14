6a2e62a9f93063f58b1f5a2acd93f0c7745a1799
jordizurita <jordi.zurita@alimentium.com>
2022-08-14 22:39:49 +0200

    template forms finished

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
+  @ViewChild('miFormularioSwitches') miFormularioSwitches!: NgForm;
 
-  ngOnInit(): void {
+  get errores(): any {
+    return this.miFormularioSwitches?.controls['terminosYcondiciones']?.errors
   }
+  
+  persona = {
+    genero: 'F',
+    notificaciones: false
+  }
+
+  terminosYcondiciones: boolean = false;
 
 }
