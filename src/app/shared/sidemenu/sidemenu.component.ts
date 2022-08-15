38bd5ab362adf7ee3fd92e6fea1a25c489217cfd
jordizurita <jordi.zurita@alimentium.com>
2022-08-15 23:08:51 +0200

    create auth module, routes and components

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -42,4 +42,14 @@ export class SidemenuComponent {
     }
   ];
 
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    },
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    }
+  ];
 }
