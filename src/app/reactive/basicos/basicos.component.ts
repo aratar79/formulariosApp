04c4bb0aff169fd42dd2bcfaee2d07a0a4ceb9dc
jordizurita <jordi.zurita@alimentium.com>
2022-08-15 17:11:00 +0200

    reactives dynamics

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['',[ Validators.required, Validators.minLength(3)]],
+    precio: [,[Validators.required, Validators.min(0)]],
+    stock: [,[Validators.required, Validators.min(0)]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  validarCampo(campo: string) {
+    return this.miFormulario.controls[campo].errors &&
+           this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    if(this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 
   ngOnInit(): void {
+    this.miFormulario.reset(
+      {nombre: 'Jordi Zurita'}
+    )
   }
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear'],
+      ['PacMan']
+    ], Validators.required)
+  });
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  validarCampo(campo: string) {
+    return this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched;
+  }
+
+  agregarFavorito() {
+
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+    this.favoritosArr.push(
+      new FormControl(
+        this.nuevoFavorito.value,
+        Validators.required
+      )
+    );
+    this.nuevoFavorito.reset();
+  }
+
+  borrar(index: number) {
+    this.favoritosArr.removeAt(index);
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+  }
+
 }
+
+
