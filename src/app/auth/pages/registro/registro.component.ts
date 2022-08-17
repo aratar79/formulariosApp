0713eef61129aa2cc6ef037c4bce4bcd25dfafed
jordizurita <jordi.zurita@alimentium.com>
2022-08-17 21:50:30 +0200

    custom errors messages

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -12,26 +13,45 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.vs.namePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)], [this.ev]],
     username: ['', [Validators.required, this.vs.noPuedeSeraratar79]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   },
-  {
-    validators: [this.vs.camposIguales('password', 'password2')] // Pasamos como argumento el nombre de las propiedades que queremos comparar en el método
-  });
+    {
+      validators: [this.vs.camposIguales('password', 'password2')] // Pasamos como argumento el nombre de las propiedades que queremos comparar en el método
+    });
+
+  get errorMailMsg(): string {
+
+    const errors = this.miFormulario.get('email')?.errors;
+
+    if (errors?.['required']) {
+      return 'El email es obligatorio.';
+    }
+    else if (errors?.['pattern']) {
+      return 'escribe un email correcto.';
+    }
+    else if (errors?.['enUso']) {
+      return 'La cuenta de correo ya esta en uso.';
+    }
+    return '';
+  }
 
   constructor
     (
       private fb: FormBuilder,
-      private vs: ValidatorService
+      private vs: ValidatorService,
+      private ev: EmailValidatorService
     ) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Jordi Zurita',
       email: 'test1@test.com',
-      username: 'aratar'
+      username: 'aratar',
+      password: '123456',
+      password2: '123456'
     });
   }
 
@@ -39,6 +59,7 @@ export class RegistroComponent implements OnInit {
     return this.miFormulario.get(campo)?.invalid &&
       this.miFormulario.get(campo)?.touched;
   }
+
 
   submitFormulario() {
     this.miFormulario.markAllAsTouched();
